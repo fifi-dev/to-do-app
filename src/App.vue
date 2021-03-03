8eaf02e7eda9e025791cb21b3612f32bef136ecf
“Fideline <fideline.bazana_ntomo@edu.devinci.fr>
2021-03-03 08:32:08 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+
+<template>
+  <div id="app">
+     <section class="hero is-warning is-bold is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">{{msg}}</h1>
+        </div>
+      </div>
+
+      <div class="hero-foot">
+        <div class="container">
+          <div class="content has-text-centered pb-5">
+            <p>
+              <strong>Atelier Mars 2021</strong>
+            </p>
+            <p>
+              Découvrez comment styliser simplement votre ToDo list avec 
+              <a class="has-text-primary" href="https://bulma.io/">
+                <strong>Bulma</strong>
+              </a> 
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello, et bienvenue sur la demo TodoList en Vue.js!'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
